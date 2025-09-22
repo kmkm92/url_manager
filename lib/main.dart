7b55dabb1a5fceefb440fbd50f753754b6e75667
Makoto Kono <71328932+kmkm92@users.noreply.github.com>
2025-09-22 19:24:32 +0900

    Initialize intl locale data before app start

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
 import 'views/url_list_view.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('ja_JP');
+
   runApp(
     ProviderScope(
       child: MyApp(),
